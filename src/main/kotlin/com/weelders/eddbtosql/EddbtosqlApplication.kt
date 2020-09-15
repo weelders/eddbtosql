7bc15e85105267241031065abd054eadb13cf6b7
weelders <40818593+weelders@users.noreply.github.com>
2020-09-15 09:17:09 +0200

    Initial commit

---
+++ src/main/kotlin/com/weelders/eddbtosql/EddbtosqlApplication.kt
@@ -0,0 +1,12 @@
+package com.weelders.eddbtosql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EddbtosqlApplication
+
+fun main(args: Array<String>)
+{
+    runApplication<EddbtosqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/weelders/eddbtosql/EddbtosqlApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.weelders.eddbtosql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EddbtosqlApplicationTests
+{
+
+    @Test
+    fun contextLoads()
+    {
+    }
+
+}
